b44468f438a2574f0a91c7ccce0f2201436db059
Krisztian Kocsis <kocsis.krisztian.akos@gmail.com>
2018-01-03 19:43:04 +0100

    Project skeleton created with Spring Initializr

---
+++ src/main/java/com/kkocsis/jsonprocessor/JsonProcessorApplication.java
@@ -0,0 +1,12 @@
+package com.kkocsis.jsonprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonProcessorApplication.class, args);
+	}
+}
