87e9346ab1f6783a277e1c9ce83a1bdb5cbf28a7
Krisztian Kocsis <kocsis.krisztian.akos@gmail.com>
2018-01-06 17:39:55 +0100

    Swagger and Swagger UI added

--- src/main/java/com/kkocsis/jsonprocessor/web/MessageController.java
+++ src/main/java/com/kkocsis/jsonprocessor/web/MessageController.java
@@ -1,13 +1,22 @@
 package com.kkocsis.jsonprocessor.web;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kkocsis.jsonprocessor.domain.Message;
 import com.kkocsis.jsonprocessor.service.MessageService;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+import java.util.List;
+
+@Slf4j
 @RestController
 @RequestMapping("/messages")
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
@@ -18,16 +27,32 @@ public class MessageController {
 
     private final StringRedisTemplate redisTemplate;
     private final MessageService messageService;
+    private final ObjectMapper objectMapper;
 
-    @PostMapping
+    @ApiOperation(value = "Post new message", notes = "Any valid JSON is acceptable, there isn't any structural restriction")
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity postJson(@RequestBody String rawMessage) {
+        if (!isValidJSON(rawMessage)) {
+            log.error("Not valid JSON: " + rawMessage);
+            return ResponseEntity.badRequest().build();
+        }
         redisTemplate.convertAndSend(redisTopicPattern, rawMessage);
         return ResponseEntity.ok().build();
     }
 
-    @GetMapping
-    public ResponseEntity findAllJson() {
-        return ResponseEntity.ok().body(messageService.findAll());
+    @ApiOperation(value = "List all messages")
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Message> findAllJson() {
+        return messageService.findAll();
+    }
+
+    private boolean isValidJSON(final String json) {
+        try {
+            objectMapper.readTree(json);
+        } catch (IOException e) {
+            return false;
+        }
+        return true;
     }
 
 }
